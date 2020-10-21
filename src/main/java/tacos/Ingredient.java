2634a26d51bb701b80764cc6edbc96c00fb3e3df
PARK WONJIN <smartwj@naver.com>
2020-10-21 19:07:40 +0900

    upto 4.1

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,12 +1,21 @@
 package tacos;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 //lombok을 통해 getter, setter 메서드 ,equals 등 유용한 메서드를 대신 작성해준다.
 @Data
 @RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE,force = true)
+@Entity
 public class Ingredient {
+
+    @Id
     private final String id;
     private final String name;
     private final Type type;
