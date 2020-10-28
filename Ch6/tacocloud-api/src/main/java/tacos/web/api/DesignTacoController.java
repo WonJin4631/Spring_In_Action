f9f3d07ab5561e8f11d52f564af974724a920512
PARK WONJIN <smartwj@naver.com>
2020-10-28 16:36:16 +0900

    upto 6.1.1

---
+++ Ch6/tacocloud-api/src/main/java/tacos/web/api/DesignTacoController.java
@@ -0,0 +1,124 @@
+//tag::recents[]
+package tacos.web.api;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+//end::recents[]
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+//tag::recents[]
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import tacos.Taco;
+import tacos.data.TacoRepository;
+
+@RestController
+@RequestMapping(path="/design",                      // /design 경로의 요청 처리
+                produces="application/json")
+@CrossOrigin(origins="*")        // 서로 다른 도메인 간의 요청을 허용
+public class DesignTacoController {
+  private TacoRepository tacoRepo;
+  
+  @Autowired
+  EntityLinks entityLinks;
+
+  public DesignTacoController(TacoRepository tacoRepo) {
+    this.tacoRepo = tacoRepo;
+  }
+
+  @GetMapping("/recent")
+  public Iterable<Taco> recentTacos() {                 // 최근 생성된 타코 디자인들을 가져와서 반환
+    PageRequest page = PageRequest.of(
+            0, 12, Sort.by("createdAt").descending());
+    return tacoRepo.findAll(page).getContent();
+  }
+  //end::recents[]
+
+//  @GetMapping("/recenth")
+//  public Resources<TacoResource> recentTacosH() {
+//    PageRequest page = PageRequest.of(
+//            0, 12, Sort.by("createdAt").descending());
+//    List<Taco> tacos = tacoRepo.findAll(page).getContent();
+//    
+//    List<TacoResource> tacoResources = 
+//        new TacoResourceAssembler().toResources(tacos);
+//    Resources<TacoResource> recentResources = 
+//        new Resources<TacoResource>(tacoResources);
+//    recentResources.add(
+//        linkTo(methodOn(DesignTacoController.class).recentTacos())
+//        .withRel("recents"));
+//    return recentResources;
+//  }
+
+  
+  
+//ControllerLinkBuilder.linkTo(DesignTacoController.class)
+//.slash("recent")
+//.withRel("recents"));
+
+  
+  
+  
+//  @GetMapping("/recenth")
+//  public Resources<TacoResource> recenthTacos() {
+//    PageRequest page = PageRequest.of(
+//            0, 12, Sort.by("createdAt").descending());
+//    List<Taco> tacos = tacoRepo.findAll(page).getContent();
+//
+//    List<TacoResource> tacoResources = new TacoResourceAssembler().toResources(tacos);
+//    
+//    Resources<TacoResource> tacosResources = new Resources<>(tacoResources);
+////    Link recentsLink = ControllerLinkBuilder
+////        .linkTo(DesignTacoController.class)
+////        .slash("recent")
+////        .withRel("recents");
+//
+//    Link recentsLink = 
+//        linkTo(methodOn(DesignTacoController.class).recentTacos())
+//        .withRel("recents");
+//    tacosResources.add(recentsLink);
+//    return tacosResources;
+//  }
+  
+  //tag::postTaco[]
+  @PostMapping(consumes="application/json")
+  @ResponseStatus(HttpStatus.CREATED)
+  public Taco postTaco(@RequestBody Taco taco) {
+    return tacoRepo.save(taco);
+  }
+  //end::postTaco[]
+  
+  
+//  @GetMapping("/{id}")
+//  public Taco tacoById(@PathVariable("id") Long id) {
+//    Optional<Taco> optTaco = tacoRepo.findById(id);
+//    if (optTaco.isPresent()) {
+//      return optTaco.get();
+//    }
+//    return null;
+//  }
+  // 위의 코드(일치하는 타코가 없다면 null을 반환하는 것)보다 HTTP 404상태 코드를 응답으로 반환하는 것이 더 좋음
+  @GetMapping("/{id}")
+  public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id) {
+    Optional<Taco> optTaco = tacoRepo.findById(id);
+    if (optTaco.isPresent()) {
+      return new ResponseEntity<>(optTaco.get(), HttpStatus.OK);
+    }
+    return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+  }
+
+  
+//tag::recents[]
+}
+//end::recents[]
+
